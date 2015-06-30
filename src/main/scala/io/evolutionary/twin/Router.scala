97b363cdb97124a6163c8542ff592a4bddda93f5
Edmund Noble <eanoble@uwaterloo.ca>
2015-06-29 21:06:50 -0400

    Added basic routing and config
    Also, added to the .gitignore

--- src/main/scala/io/evolutionary/twin/Router.scala
+++ src/main/scala/io/evolutionary/twin/Router.scala
@@ -1,17 +1,21 @@
 package io.evolutionary.twin
 
 import com.typesafe.config.{ConfigFactory, Config}
-import org.http4s.{Response, Request}
+import org.http4s.{Uri, Response, Request}
 import org.http4s.dsl._
 
 import scalaz.concurrent.Task
 
 import scala.collection.JavaConverters._
 
-class RouterSettings(config: Config)  {
+class RouterSettings(config: Config) {
   val routerCfg = config.getConfig("io.evolutionary.twin.router")
-  val mirroredConfigs = routerCfg.getList("mirrored").asScala
-  val
+  val mirroredConfigs = routerCfg.getConfigList("mirrored").asScala
+  val mirroredSites = for {
+    cfg <- mirroredConfigs
+    siteName = cfg.getString("site")
+    urls = cfg.getStringList("urls").asScala.map(url => Uri.fromString(url).fold((err) => throw new RuntimeException(s"Invalid URL: $url. Error: $err"), identity))
+  } yield Site(siteName, urls)
 }
 
 object Router {
@@ -20,8 +24,19 @@ object Router {
 
 class Router(settings: RouterSettings) extends PartialFunction[Request, Task[Response]] {
   override def isDefinedAt(x: Request): Boolean = x match {
-    case GET -> Root / (site: String) if settings.mirroredSites.contains(site) =>
-      Sites.getRoute(site, x.body)
+    case GET -> Root / (site: String) =>
+      val targetSite = settings.mirroredSites.find(_.name == site)
+      targetSite.isDefined
   }
-  override def apply(v1: Request): Task[Response] = ???
+  override def apply(v1: Request): Task[Response] = v1 match {
+    case GET -> Root / (site: String) =>
+      val targetSite = settings.mirroredSites.find(_.name == site)
+      val url = v1.params.get("url")
+      val task = for {
+        site <- targetSite
+        urlRaw <- url
+        urlParsed <- Uri.fromString(urlRaw).toOption
+      } yield Sites.getRoute(site, urlParsed)
+      task.fold(NotFound())(Ok(_))
+  }
 }
--- src/main/scala/io/evolutionary/twin/Sites.scala
+++ src/main/scala/io/evolutionary/twin/Sites.scala
@@ -1,19 +1,47 @@
 package io.evolutionary.twin
 
+import java.io.File
+import java.nio.file.Files
+
 import org.http4s.client.Client
 import org.http4s.dsl._
 import org.http4s._
 
+import scala.io.Codec
 import scalaz.concurrent.Task
+import scalaz._
+import scalaz.stream._
+import Scalaz._
 
-case class Site(name: String, urls: List[Uri])
+case class Site(name: String, urls: Seq[Uri])
 
 object Sites {
-  def getRoute(url: Uri): Task[String] = {
-    
+  val validFilenameCharacters = ('0' to '9') ++ ('A' to 'Z')
+
+  def getRoute(site: Site, url: Uri): Process[Task, String] = {
+    val cachedAlready = isCached(site, url)
+    if (cachedAlready) {
+      retrieveFile(site, url)
+    } else {
+      fetchSite(url)
+    }
   }
 
-  def fetchSite(site: Site)(implicit client: Client): Seq[Task[String]] = {
-    site.urls.map(url => client(url).as[String])
+  def retrieveFile(site: Site, url: Uri): Process[Task, String] = {
+    val fileName = routeToFileName(site, url)
+    io.linesR(fileName)(Codec.UTF8)
+  }
+
+  def isCached(site: Site, url: Uri): Boolean = {
+    new File(routeToFileName(site, url)).exists()
+  }
+
+  def routeToFileName(site: Site, url: Uri): String = {
+    def escapeFileName(name: String): String = name.filter(validFilenameCharacters.contains)
+    Vector(site.name.toUpperCase, url.toString().toUpperCase).map(escapeFileName).mkString(File.separator)
+  }
+
+  def fetchSite(url: Uri)(implicit client: Client): Process[Task, String] = {
+    Process.eval(client(url).as[String])
   }
 }
